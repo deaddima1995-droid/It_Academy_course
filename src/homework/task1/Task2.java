131ff10d16383e6ef0c5b821877c88a893486305
deaddima1995-droid <zhyravlevdima@yandex.com>
2022-01-29 12:52:35 +0300

    Create Task1.java

--- src/homework/task1/Task2.java
+++ src/homework/task1/Task2.java
@@ -11,25 +11,22 @@ https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D0%A4%D0%B8%D0%B1%
 package homework.task1;
 
 public class Task2 {
+
     public static void main(String[] args) {
-        int num = 100;
-        int sum = funOfFibonachi(10,0,0);
-        System.out.println(sum);
+        int index = 15;
+        int sum = 0;
+        int number = 1;
+        int nextNumber = 0;
+
+        for (int i = 1; i <= index; i++) {
+                int tmp = number + nextNumber;
+                number = nextNumber;
+                nextNumber = tmp;
+                if (number % 2 == 0) {
+                    sum += number;
+                }
+        }
+        System.out.println("Сумма = "+sum);
     }
-    public static int funOfFibonachi(int index, int number,int tmp) {
-        if (index <= 0) {
-            return number;
-        }
 
-        number += tmp;
-
-        if (number == 0) {
-            number++;
-        }
-        else {
-            tmp += number;
-        }
-        return number += funOfFibonachi(--index,number,tmp);
-
-    }
 }
