ba731e16d98f62df78c00490bcd37fec33b835e4
deaddima1995-droid <zhyravlevdima@yandex.com>
2022-02-03 20:04:02 +0300

    Create Task1.java

--- src/homework/task1/Task7.java
+++ src/homework/task1/Task7.java
@@ -12,15 +12,19 @@ package homework.task1;
 public class Task7 {
 
     public static void main(String[] args) {
-        int[] array = {9,10,10,4,0,10,0,1,7,0,2,25};
+        int[] array = new int[10];
+        Task1.pasteRandomNumber(array);
+
         int[] temp = sumMaxAdjacentNumbers(array);
-        //Task1.pasteRandomNumber(array);
-       Task1.writeArray(array);
-        Task1.writeArray(sumMaxAdjacentNumbers(array));
-        //makeZero(array,2);
-        //Task1.writeArray(array);
-        int our = sumMaxArrayWithOutAdjacentNumbers(array,temp);
-        System.out.println(our);
+
+
+        System.out.print("Наш массив\t\t\t");
+        Task1.writeArray(array);
+        System.out.print("Масса его элементов\t");
+        Task1.writeArray(temp);
+
+        int sum = sumMaxArrayWithOutAdjacentNumbers(array,temp);
+        System.out.println("Максимальная сумма для данного массива =\t"+sum);
 
     }
     public static int sumMaxArrayWithOutAdjacentNumbers(int[] array,int[] temp) {
@@ -28,7 +32,7 @@ public class Task7 {
         if (max(array) == 0) {
             return 0;
         }
-        int maxTempIndex = maxOfArray(temp);
+        int maxTempIndex = maxOfArrayByIndex(temp);
         sum += array[maxTempIndex];
         makeZero(temp, maxTempIndex);
         makeZero(array,maxTempIndex);
@@ -42,7 +46,7 @@ public class Task7 {
             int max = Integer.MIN_VALUE;
 
             for (int j = i; j < array.length; j++) {
-                if (j == i + 1 | j== i - 1 | j == i){
+                if (j == i + 1 | j == i){
                     continue;
                 }
                 int tmp = max;
@@ -52,7 +56,7 @@ public class Task7 {
                 }
             }
         }
-//лево
+//влево
         for (int i = array.length - 1; i >= 0; i--) {
             int max = Integer.MIN_VALUE;
 
@@ -75,12 +79,12 @@ public class Task7 {
 
     public static int max(int[] array) {
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < array.length; i++) {
-            max = Math.max(max,array[i]);
+        for (int j : array) {
+            max = Math.max(max, j);
         }
         return max;
     }
-    public static int maxOfArray(int[] array) {
+    public static int maxOfArrayByIndex(int[] array) {
         int max = Integer.MIN_VALUE;
         int index = 0;
         for (int i = 0; i < array.length; i++) {
