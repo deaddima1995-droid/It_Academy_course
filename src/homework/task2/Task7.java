d8add50f3b33ec48ab65d46219a8958f43dde30c
deaddima1995-droid <zhyravlevdima@yandex.com>
2022-02-06 12:41:58 +0300

    Я сделяль

--- src/homework/task2/Task7.java
+++ src/homework/task2/Task7.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 public class Task7 {
     public static void main(String[] args) {
-        String text = "Вода Анаконда Алла Автобус Аэрограф Africa";
-        Pattern pattern = Pattern.compile("[аАaA][а-яА-Яa-zA-Z]+?[аa]\\b");
+        String text = "Вода Анаконда Алла Автобус Аэрограф Africa аA";
+        Pattern pattern = Pattern.compile("([аАaA][а-яА-Яa-zA-Z]+?[аaАA]\\b)|([aAаА][аАaA]\\b)");
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             System.out.println(text.substring(matcher.start(), matcher.end()));
