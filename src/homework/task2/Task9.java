22a861e1103d2c580ca4493791a1e9c5f668b428
deaddima1995-droid <zhyravlevdima@yandex.com>
2022-02-07 21:39:52 +0300

    Я сделяль

--- src/homework/task2/Task9.java
+++ src/homework/task2/Task9.java
@@ -7,7 +7,6 @@ public class Task9 {
     public static void main(String[] args) {
         String text = "Hello world";
         String word = "worlD";
-        String string = "i";
         Pattern searchWord = Pattern.compile(word.toLowerCase());   // или так ("(?i)"+word) , но не работает с русским языком
         Matcher matcher = searchWord.matcher(text.toLowerCase());
         while (matcher.find()) {
