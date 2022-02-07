1271c246da6d90b41272af8263421c89f1084868
deaddima1995-droid <zhyravlevdima@yandex.com>
2022-02-07 22:58:05 +0300

    Я сделяль

--- src/homework/task2/Task9.java
+++ src/homework/task2/Task9.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 
 public class Task9 {
     public static void main(String[] args) {
-        String text = "Hello world";
+        String text = "Hello worldworld world";
         String word = "worlD";
-        Pattern searchWord = Pattern.compile(word.toLowerCase());   // или так ("(?i)"+word) , но не работает с русским языком
+        Pattern searchWord = Pattern.compile("\\b"+word.toLowerCase()+"\\b");   // или так ("(?i)"+word) , но не работает с русским языком
         Matcher matcher = searchWord.matcher(text.toLowerCase());
         while (matcher.find()) {
             System.out.println("Индекс слова "+word+" -\t"+matcher.start());
