a72e16b78714be8cef38d0313535d1f8848f61c4
deaddima1995-droid <zhyravlevdima@yandex.com>
2022-02-08 19:27:31 +0300

    Create Task1.java

--- src/homework/task2/Task9.java
+++ src/homework/task2/Task9.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 
 public class Task9 {
     public static void main(String[] args) {
-        String text = "Hello worldworld world";
-        String word = "worlD";
-        Pattern searchWord = Pattern.compile("\\b"+word.toLowerCase()+"\\b");   // или так ("(?i)"+word) , но не работает с русским языком
-        Matcher matcher = searchWord.matcher(text.toLowerCase());
+        String text = "Hello worldworld world ПРИВЕТ";
+        String word = "WORLD";
+        Pattern searchWord = Pattern.compile("\\b"+word+"\\b",Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);   // или так ("(?i)"+word) , но не работает с русским языком
+        Matcher matcher = searchWord.matcher(text);
         while (matcher.find()) {
             System.out.println("Индекс слова "+word+" -\t"+matcher.start());
         }
--- src/homework/task2/addition/Box.java
+++ src/homework/task2/addition/Box.java
@@ -1,5 +1,8 @@
 package homework.task2.addition;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Box {
     private final Integer length;
     private final Integer width;
@@ -23,6 +26,15 @@ public class Box {
             this.type = "Cube";
         }
         public Box(String configuration) {
+            Pattern finder = Pattern.compile("(?i)Box[\\d{0,3]");
+            Matcher matcher = finder.matcher(configuration);
+            String boxer = matcher.group();
+            int[] data = new int[3];
+            int index = 0;
+            while (matcher.find()) {
+                data[index] = matcher.start();
+                index++;
+            }
             this.length = 0;
             this.width = 0;
             this.height = 0;
