9376818d33ab62a57698b9fd6bd6c1833a6f5756
deaddima1995-droid <zhyravlevdima@yandex.com>
2022-02-10 21:11:17 +0300

    Create Task1.java

--- src/homework/task2/Task7.java
+++ src/homework/task2/Task7.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 public class Task7 {
     public static void main(String[] args) {
-        String text = "Вода Анаконда Алла Автобус Аэрограф Africa аA";
-        Pattern pattern = Pattern.compile("([аАaA][а-яА-Яa-zA-Z]+?[аaАA]\\b)|([aAаА][аАaA]\\b)");
+        String text = "Вода Анаконда Алла Автобус Аэрограф Africa аA a а";
+        Pattern pattern = Pattern.compile("\\b[aAаА]((\\w+?)?([aAаА]))?\\b", Pattern.UNICODE_CHARACTER_CLASS);
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
             System.out.println(text.substring(matcher.start(), matcher.end()));
